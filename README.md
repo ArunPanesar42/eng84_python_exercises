d9cf485cc557e4440526adfcbf0283964d8815b8
AyPeeEatsDaal <p17195823@my365.dmu.ac.uk>
2021-03-24 17:02:55 +0000

    Added basic code to Task one and updates README

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Python Independent exercises
+### Exercise 1
+- Ask user name and age
+- Calculate which year the user was born
+- Print "Omg (name), you are (age) years old so you were born in (year)"
+- Prompt user to reassign name and age
+- Find out if the person has had their birthday this year
---
+++ python_exercise1.py
@@ -0,0 +1,10 @@
+# Exercise 1
+# Calculate year of birth
+import datetime
+
+name = input("Hello, What is your name?")  #Gets User to input date
+age = int(input("How old are You?")) #Gets user to input age
+birth_year = (2021 - age) #since the current year is 2021 we minus that from age to get birth year
+
+print(f"OMG {name}, you are {age} years old so you were born in {birth_year}")
+# This isnt accurate because the person may be born later in the year so it will get their year of birth wrong.
